b67a86734e37bcff9f865e08baaa022f86f00a2a
jefryhdz <jefryhdz@gmail.com>
2017-11-24 14:21:23 -0600

    cambios

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-#include <iostrea>
+#include <iostream>
 #include <vector>
-#include "persona"
+#include "persona.h"
 
 	
 using namespace std;
 
 int menu();
+persona* agregar();
 
-
+vector <persona*> lista;
 
 int main(){
 	int opcion =-1;
@@ -16,7 +17,8 @@ int main(){
 		opcion= menu();
 		if (opcion==1)
 		{
-			
+			persona* person = agregar();
+				
 		}else if (opcion==2)
 		{
 			
@@ -30,13 +32,83 @@ int main(){
 }
 
 int menu(){
-	int resp !=-1;
+	int resp =-1;
 	do
 	{
 		cout<<"1. Agregar Personas"<<endl;	
 		cout<<"2. Eliminar"<<endl;
 		cout<<"3. Salir"<<endl;
 		cin>>resp;
-	} while (resp <0||resp>3);
+	} while (resp <0&&resp>3);
 	return resp;
+}
+
+persona* agregar(){
+	int cabello,ojo,piel;
+	char genero;
+	bool fertil;
+	string nombre;
+	cout<<"------Creacion de persona---"<<endl;
+	int resp=-1;
+	cout<<"Ingrese el nombre de la persona"<<endl;
+	cin>>nombre;
+	do
+	{	
+		cout<<"Ingrese el numero del color de cabello"<<endl;
+		cout<<"1. Negro"<<endl;
+		cout<<"2. Cafe"<<endl;
+		cout<<"3. Rubio"<<endl;
+		cin>>resp;
+	} while (resp<1&&resp>3);
+	cabello = resp;
+	resp=-1;
+	do
+	{	
+		cout<<"Ingrese el numero del color de ojos"<<endl;
+		cout<<"1. Azul"<<endl;
+		cout<<"2. Negro"<<endl;
+		cout<<"3. Cafe"<<endl;		
+		cin>>resp;
+	} while (resp<1&&resp>3);
+	ojo = resp;
+	resp=-1;
+	do
+	{	
+		cout<<"Ingrese el numero del piel"<<endl;
+		cout<<"1. Blanco"<<endl;
+		cout<<"2. Negro"<<endl;
+		cout<<"3. Trigueño"<<endl;		
+		cin>>resp;
+	} while (resp<1&&resp>3);
+	piel= resp;
+	resp=-1;
+	do
+	{	
+		cout<<"Ingrese el numero de sexo"<<endl;
+		cout<<"1. Masculino"<<endl;
+		cout<<"2. Femenino"<<endl;	
+		cin>>resp;
+	} while (resp<1&&resp>2);
+	if (resp ==1)
+	{
+		genero='M';
+	}else{
+		genero='F';
+	}
+	resp=-1;
+	do
+	{	
+		cout<<"Fertil"<<endl;
+		cout<<"1. si"<<endl;
+		cout<<"2. no"<<endl;	
+		cin>>resp;
+	} while (resp<1&&resp>2);	
+	if (resp==1)
+	{
+		genero = true;
+	}else{
+		genero =false;
+	}
+	persona* person = new persona(nombre,genero,cabello,ojo,piel,fertil);
+	return person;
 }
